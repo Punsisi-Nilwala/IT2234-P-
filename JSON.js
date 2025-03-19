f9e327c3b2ce65d0daad0c90cbe39444f13e75a9
Punsisi-Nilwala <punsisinilwala@gmail.com>
2025-03-19 16:12:19 +0530

    JSON

---
+++ JSON.js
@@ -0,0 +1,15 @@
+//JSON
+//{key:value}
+
+
+
+let student={regno:'2021/ICT/33',name:'Punsisi',age:22,course:'IT',skills:['java','JS','c++']}
+console.log(student)
+console.log(student.name)
+
+let students=[
+{regno:'2021/ICT/33',name:'Punsisi',age:22,course:'IT'},
+{regno:'2021/ICT/34',name:'John',age:23,course:'IT'},
+{regno:'2021/ICT/35',name:'James',age:21,course:'IT'}
+]
+console.log(students)
